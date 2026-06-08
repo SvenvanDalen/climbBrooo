dc758a9560545e4e3f4d2cd4fea1c6481599488b
Sven van Dalen <b.l.dalen@student.tudelft.nl>
2026-06-08 21:33:13 +0200

    feat(android): wire WatchRequestHandler into ConnectIqClient

--- android/app/src/main/java/nl/paree/climbpro/ClimbProApplication.java
+++ android/app/src/main/java/nl/paree/climbpro/ClimbProApplication.java
@@ -3,6 +3,9 @@ package nl.paree.climbpro;
 import android.app.Application;
 import org.osmdroid.config.Configuration;
 import java.io.File;
+import nl.paree.climbpro.connectiq.ConnectIqClient;
+import nl.paree.climbpro.connectiq.WatchRequestHandler;
+import nl.paree.climbpro.data.route.RouteRepository;
 
 public final class ClimbProApplication extends Application {
     @Override
@@ -11,5 +14,9 @@ public final class ClimbProApplication extends Application {
         Configuration.getInstance().setUserAgentValue("ClimbPro/1.0");
         Configuration.getInstance().setOsmdroidTileCache(
                 new File(getCacheDir(), "osmdroid"));
+
+        ConnectIqClient ciqClient = new ConnectIqClient(this);
+        RouteRepository routeRepo = new RouteRepository(this);
+        ciqClient.setWatchRequestHandler(new WatchRequestHandler(routeRepo, ciqClient));
     }
 }
